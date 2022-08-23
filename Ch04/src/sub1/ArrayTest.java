12699925df1590242bc7f80b824a00f50060f393
seonjuHa <gktjswn15@gmail.com>
2022-08-23 16:26:00 +0900

    2022/08/23 제4장 메서드 학습

--- Ch04/src/sub1/ArrayTest.java
+++ Ch04/src/sub1/ArrayTest.java
@@ -96,8 +96,8 @@ public class ArrayTest {
 		
 		for(int a = 0; a < 3; a++) {
 			for(int b = 0; b < 3; b++) {
-				for(int c = 0; b < 3; c++) {
-					System.out.printf("arr3d[%d][%d][%d] : %d\n " , a, b, c, arr3d[a][b][c]);
+				for(int c = 0; c < 3; c++) {
+					System.out.printf("arr3d[%d][%d][%d] : %d\n" , a, b, c, arr3d[a][b][c]);
 				
 				}				
 			}
