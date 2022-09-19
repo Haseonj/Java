7a345c08b422be8d59e6c2d5f11e38767117d23a
seonjuHa <gktjswn15@gmail.com>
2022-09-20 01:08:58 +0900

    2022/09/19 at home

---
+++ book1/src/Ch07/P208BookArray.java
@@ -0,0 +1,11 @@
+package Ch07;
+
+public class P208BookArray {
+	public static void main(String[] args) {
+		P207Book[] library = new P207Book[5];
+		
+		for(int i = 0; i < library.length; i++) {
+			System.out.println(library[i]);
+		}
+	}
+}
