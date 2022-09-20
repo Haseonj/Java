9de8bfa1fd1f6e34bf30a39dd4efb7119c0d3998
Haseonj <111490142+Haseonj@users.noreply.github.com>
2022-09-20 19:30:53 +0900

    Delete SelfStudy directory

--- SelfStudy/src/Ch01/P033.java
+++
@@ -1,7 +0,0 @@
-package Ch01;
-
-public class P033 {
-	public static void main(String[] args) {
-		System.out.println("Hello, Java");
-	}
-}
--- SelfStudy/src/Ch05/Person.java
+++
@@ -1,9 +0,0 @@
-package Ch05;
-
-public class Person {
-	String name; // 참조자료형 지금 만든 person 이라는 클래스도 참조자료형이 될 수 있다.
-	int height;
-	double weight;
-	char gender;
-	boolean married;
-}
