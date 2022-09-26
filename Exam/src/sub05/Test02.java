be62cad12a4209fa8319bbeb76b3ebef24817274
seonjuHa <gktjswn15@gmail.com>
2022-09-26 16:44:26 +0900

    2022/09/26

---
+++ Exam/src/sub05/Test02.java
@@ -0,0 +1,23 @@
+package sub05;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 4;
+		int c = -21;
+		
+		int x1 = (-b + (int) Math.sqrt(b*b -4*a*c)) / 2 * a; // Math.sqrt : 제곱근
+		int x2 = (-b - (int) Math.sqrt(b*b -4*a*c)) / 2 * a;
+		
+		boolean result1 = a* Math.pow(x1, 2) + b * x1 + c == 0; //Math.pow : 제곱
+		boolean result2 = a* Math.pow(x1, 2) + b * x1 + c == 0;
+		
+		System.out.println("x1 : " + x1);
+		System.out.println("x2 : " + x2);
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : " + result2);
+	
+		
+	}
+}
