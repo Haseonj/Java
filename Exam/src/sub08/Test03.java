2c3d4d81619ec7e4b41c7eb34725d57e3bb44dff
seonjuHa <gktjswn15@gmail.com>
2022-09-30 19:25:05 +0900

    2022/09/30

--- Exam/src/sub08/Test03.java
+++ Exam/src/sub08/Test03.java
@@ -18,5 +18,12 @@ class Member {
 }
 
 public class Test03 {
-	
+	public static void main(String[] args) {
+		Member m1 = new Member("김유신", "010-1234-1001", 25);
+		Member m2 = new Member("김춘추", "010-1234-1002", 23);
+		
+		System.out.println(m1);
+		System.out.println(m2);
+		
+	}
 }
--- Exam/src/sub08/Test08.java
+++ Exam/src/sub08/Test08.java
@@ -1,5 +1,43 @@
 package sub08;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class Person {
+	private String pid;
+	private String name;
+	private int age;
+	
+	public String getPid() {return pid;};
+	public String getName() {return name;};
+	public int getAge() {return age;};
+	
+	public Person(String pid, String name, int age) {
+		this.pid = pid;
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("아이디 : %s, 이름 : %s, 나이 : %d", pid, name, age);
+	}
+}
+
+class PersonMap {
+	private Map<String, Person> persons;
+	
+	public PersonMap() {
+		persons = new HashMap<>();
+	}
+	
+	public void addPerson(Person p) {
+		persons.put(p.getPid(), p);
+	}
+	
+	
+}
+
 public class Test08 {
 
 }
